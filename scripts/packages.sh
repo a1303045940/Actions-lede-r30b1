550eb1c9f4aaa53b7631d58acba6a7a226b6220e
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-24 01:56:05 +0800

    Comment out ZeroTier service name adjustments
    Comment out sed commands for ZeroTier adjustments

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -80,6 +80,6 @@ popd
 echo "packages executed successfully!"
 
 # 调整 ZeroTier 到 服务 菜单
-sed -i 's/vpn/services/g; s/VPN/Services/g' feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
+#sed -i 's/vpn/services/g; s/VPN/Services/g' feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+#sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
