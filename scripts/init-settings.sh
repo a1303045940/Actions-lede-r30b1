6d0d7e3699d8daf766ab3e19894f3b5cdad958e6
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-23 21:52:07 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -12,14 +12,23 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 #修改默认WIFI名
 #sed -i "s/\.ssid=.*/\.ssid=Openwrt/g" $(find ./package/kernel/mac80211/ ./package/network/config/ -type f -name "mac80211.*")
 
-# 将默认 SSID 改为根据频段区分
-sed -i 's/set wireless\.default_radio\${devidx}\.ssid=LEDE/set wireless.default_radio${devidx}.ssid=$([ "$mode_band" = "2g" ] \&\& echo "OpenWrt-2.4" || echo "OpenWrt5G")/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #修改WIFI加密
 # 修改默认wifi密码key为password
 sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改WIFI密码
+
+
+# 修改加密方式从 none 改为 psk2
+#sed -i 's/set wireless\.default_radio\${devidx}\.encryption=none/set wireless.default_radio${devidx}.encryption=psk2/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改 SSID 为根据频段区分
+sed -i 's/set wireless\.default_radio\${devidx}\.ssid=LEDE/set wireless.default_radio${devidx}.ssid=$([ "$mode_band" = "2g" ] \&\& echo "OpenWrt-2.4G" || echo "OpenWrt-5G")/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 添加wifi密码
+#sed -i '/set wireless\.default_radio\${devidx}\.encryption=psk2/a\\t\tset wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 
 
 # 最大连接数修改为65535
