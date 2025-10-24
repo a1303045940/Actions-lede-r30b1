1636b1a1714cf4b4643963768b9f6733e40ad8eb
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-24 14:13:20 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -70,7 +70,7 @@ echo "init-settings executed successfully!"
 
 
 # 注释原行（精确匹配原URL和版本）
-#sed -i '/src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23.05/s/^/#/' "feeds.conf.default"
+sed -i '/src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23.05/s/^/#/' "feeds.conf.default"
 # 添加新行到文件末尾
 #echo "src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10" >> "feeds.conf.default"
 
