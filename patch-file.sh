a742103d8880683d912646342a872407aacba3dd
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-25 01:41:45 +0800

    Add patch-file.sh script for applying patches

---
+++ patch-file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ECHO() {
+  echo "[$(date "+%H:%M:%S")] $*"
+}
+
+function Patch_File() {
+  parent_path=$1
+  patch_file_path=$2
+  base_path=$PWD
+  cp -r "../Patch-Files/$patch_file_path" "$parent_path"
+  cd "$parent_path"
+  for packagepatch in "$patch_file_path"/*; do
+    if [ -f "$packagepatch" ]; then
+      patch_file=$(basename "$packagepatch")
+      ECHO Applying Patch File "$patch_file"
+      patch -p1 --no-backup-if-mismatch < "$patch_file_path/$patch_file"
+    fi
+  done
+  cd "$base_path"
+}
+
+# Patch_File feeds/packages feeds-package-patch-files
