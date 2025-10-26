d43998b22f7d9378f1dc16f5a4b1980d64f3176e
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-10-26 20:11:09 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -18,6 +18,7 @@ rm -rf ./feeds/packages/net/xray-core
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-passwall2
 rm -rf ./feeds/luci/applications/luci-app-openclash
+rm -rf ./feeds/luci/applications/luci-app-ddns
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
